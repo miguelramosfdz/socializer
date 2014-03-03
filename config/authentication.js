19a31ea5c1ca5702ad554a58b7eb302bfad5e8c5
CJ Ponti <cjponti@gmail.com>
2014-03-03 01:20:24 +0000

    Set up facebook authentication

---
+++ config/authentication.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var User = require('../app/model/User');
+var Auth = require('./auth');
+
+exports.setup = function(passport) {
+
+	passport.serializeUser(function(user, done) {
+		done(null, user.id);
+	});
+
+	// used to deserialize the user
+	passport.deserializeUser(function(id, done) {
+		User.findById(id, function(err, user) {
+			done(err, user);
+		});
+	});
+
+	passport.use(new FacebookStrategy({
+		clientID        : Auth.Facebook.appId,
+		clientSecret    : Auth.Facebook.appSecret,
+		callbackURL     : Auth.Facebook.callbackURL
+	},
+	function(token, refreshToken, profile, done) {
+
+		process.nextTick(function() {
+			// Find a user in the database based on their facebook id
+			User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+				// Return error if one occurs
+				if (err)
+					return done(err);
+				// if the user is found, then log them in
+				if (user) {
+					// Return user if user is found
+					return done(null, user);
+				} else {
+					// Create new user if no user exists
+					var newUser = new User();
+
+					// Save user's Facebook id
+					newUser.facebook.id = profile.id;
+
+					// Set all of the facebook information in our user model
+					newUser.facebook.profile = profile;
+
+					 // Save user's Facebook token
+					newUser.facebook.token = token;
+
+					 // Save user
+					newUser.save(function(err) {
+						if (err)
+							throw err;
+
+						/* if successful, return the new user */
+						return done(null, newUser);
+					});
+				}
+
+			});
+		});
+
+	}));
+
+
+
+};
--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,51 @@
 'use strict';
 
-exports.setup = function(app) {
+exports.setup = function(app, passport) {
 
-  app.get('/', function(req, res){
-    res.render('layout', { title: 'Express' });
-  });
+	app.get('/', function(req, res){
+		res.render('layout', { title: 'Ehxe' });
+	});
 
-  app.get('/partials/:type/:file', function(req, res) {
-    res.render('/partials'+req.params.type+'/'+req.params.file);
-  });
+	app.get('/partials/:type/:file', function(req, res) {
+		res.render('/partials'+req.params.type+'/'+req.params.file);
+	});
+
+	// route middleware to make sure a user is logged in
+	this.isLoggedIn = function(req, res, next) {
+		if (req.isAuthenticated()) {
+			// if user is authenticated in the session, carry on
+			return next();
+		} else {
+			// if they aren't redirect them to the home page
+			res.redirect('/');
+		}
+	};
+
+	// route for showing the profile page
+	app.get('/profile', this.isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user // get the user out of session and pass to template
+		});
+	});
+
+	/**
+	 * Facebook Routes
+	 */
+
+	// Route for Facebook authentication and login
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	// Route to handle the Facebook authentication callback
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	// Route for logout
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
 
 };
