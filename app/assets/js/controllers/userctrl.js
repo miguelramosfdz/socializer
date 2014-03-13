3ad4c5aeabd1143c22eb88a47af45af387f6b2df
CJ Ponti <cjponti@gmail.com>
2014-03-13 08:20:49 +0000

    Fixes to sign up form

--- app/assets/js/controllers/userctrl.js
+++ app/assets/js/controllers/userctrl.js
@@ -1,19 +1,21 @@
 app.controller('SignUpCtrl', [ '$scope', '$location', '$http',
 	function($scope, $location, $http) {
+
 		$scope.user = {
 			email: null,
 			password: null,
 			passwordConfirmation: null
-		}
+		};
 
 		$scope.signUp = function() {
-			$http({ method: 'GET', url:'/users'})
+			$http({ method: 'POST', url:'/users', data: $scope.user })
 				.success(function(data, status, headers, config) {
 					console.log(data);
 				})
 				.error(function(data, status, headers, config) {
 					console.log(data);
 				});
-		}
+		};
+
 	}
 ]);
