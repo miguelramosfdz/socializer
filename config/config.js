19b3535f01b16a8b5f8963bd4742467ef1fdc6d9
CJ Ponti <cjponti@gmail.com>
2014-04-27 21:56:54 +0100

    Fixes #5

--- config/config.js
+++ config/config.js
@@ -3,10 +3,14 @@ exports = module.exports = (function() {
 	return {
 		oauth: {
 			Facebook: {
-
+				appId: process.env.FACEBOOK_APP_ID,
+				appSecret: process.env.FACEBOOK_APP_SECRET,
+				callbackURL: process.env.FACEBOOK_CALLBACK_URL
 			},
 			Twitter: {
-
+				consumerKey: process.env.TWITTER_CONSUMER_KEY,
+				consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+				callbackURL: process.env.TWITTER_CALLBACK_URL
 			}
 		},
 		db: {
