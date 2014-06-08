cec811372d138cd471d0498b054a647b0d09b625
CJ Ponti <cjponti@gmail.com>
2014-06-08 23:24:50 +0100

    Remove SignUpController and unused UserFactory functions

--- client/scripts/factories/UserFactory.js
+++ client/scripts/factories/UserFactory.js
@@ -1,28 +1,4 @@
-angular.module('hogar').factory('UserFactory', function($http, $route) {
+angular.module('hogar').factory('UserFactory', function() {
 
-  var _this = this;
-
-  this.user = null;
-
-  return {
-
-    user: _this.user,
-
-    authenticate: function() {
-      $http({ method: 'GET', url: '/api/user' })
-        .success(function(data,status, headers, config) {
-          _this.user = result;
-        })
-    },
-
-    logIn: function(user) {
-      $http({ method: 'POST', url:'/login', data: user })
-    },
-
-    signUp: function(user) {
-      $http({ method: 'POST', url:'/signup', data: user })
-    }
-
-  };
 
 });
