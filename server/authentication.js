e7e363ab4d5215d5af9a4ed7fa3107679914b407
CJ Ponti <cjponti@gmail.com>
2014-06-08 14:59:00 +0100

    Remove passport-twitter and passport-facebook

--- server/authentication.js
+++ server/authentication.js
@@ -2,14 +2,11 @@ exports = module.exports = (function() {
 	'use strict';
 
 	var LocalStrategy = require('passport-local').Strategy;
-	var FacebookStrategy = require('passport-facebook').Strategy;
-	var TwitterStrategy = require('passport-twitter').Strategy;
 	var User = require('../app/models/User');
-	var Config = require('./config');
 
 	return {
 		csrf:  function(req) {
-			return (req.body && req.body._csrf) || (req.query && req.query._csrf) || 
+			return (req.body && req.body._csrf) || (req.query && req.query._csrf) ||
 				(req.headers['x-csrf-token']) || (req.headers['x-xsrf-token']);
 		},
 
@@ -58,73 +55,6 @@ exports = module.exports = (function() {
 						return done(null, false, { message: 'Incorrect password.' });
 					}
 					return done(null, user);
-				});
-			}));
-
-			// For 'Sign up with Facebook'
-			passport.use(new FacebookStrategy({
-				clientID: Config.oauth.Facebook.appId,
-				clientSecret: Config.oauth.Facebook.appSecret,
-				callbackURL: Config.oauth.Facebook.callbackURL
-			},
-			function(token, refreshToken, profile, done) {
-				process.nextTick(function() {
-					// Find a user in the database based on their facebook id
-					User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-						// Return error if one occurs
-						if (err)
-							return done(err);
-						// if the user is found, then log them in
-						if (user) {
-							// Return user if user is found
-							return done(null, user);
-						} else {
-							// Create new user if no user exists
-							new User({
-									name: profile.displayName,
-									email: profile.emails[0].value,
-									username: profile.username,
-									provider: 'facebook',
-									facebook: profile._json
-								})
-								.save(function(err, newUser) {
-									if (err) {
-										throw err;
-									}
-									// if successful, return the new user
-									return done(null, newUser);
-								});
-						}
-					});
-				});
-
-			}));
-
-			// For 'Sign up with Twitter'
-			passport.use(new TwitterStrategy({
-				consumerKey: Config.oauth.Twitter.consumerKey,
-				consumerSecret: Config.oauth.Twitter.consumerSecret,
-				callbackURL: Config.oauth.Twitter.callbackURL
-			},
-			function(token, tokenSecret, profile, done) {
-				User.findOne({ 'twitter.id_str': profile.id }, function(err, user) {
-					if (err) {
-						return done(err);
-					}
-					if (user) {
-						return done(null, user);
-					} else {
-						new User({
-								name: profile.displayName,
-								username: profile.username,
-								provider: 'twitter',
-								twitter: profile._json
-							})
-							.save(function(err) {
-								if (err) console.log(err);
-								return done(err, user);
-							});
-					}
 				});
 			}));
 		}
