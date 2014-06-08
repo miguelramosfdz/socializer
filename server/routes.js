f65d4bb0a42f8807e6f62d421fad99a8e271debb
CJ Ponti <cjponti@gmail.com>
2014-06-08 17:06:03 +0100

    Remove facebook and twitter passport routes

--- server/routes.js
+++ server/routes.js
@@ -2,62 +2,42 @@
 
 exports.setup = function(app, passport) {
 
-	// Route for serving templates
-	app.get('/partials/:type/:file', function(req, res) {
-		res.render('partials/'+req.params.type+'/'+req.params.file);
-	});
+  // Route for serving templates
+  app.get('/partials/:type/:file', function(req, res) {
+    res.render('partials/'+req.params.type+'/'+req.params.file);
+  });
 
-	// route middleware to make sure a user is logged in
-	this.isLoggedIn = function(req, res, next) {
-		if (req.isAuthenticated()) {
-			// if user is authenticated in the session, carry on
-			next();
-		} else {
-			// if they aren't redirect them to the home page
-			res.send(401);
-		}
-	};
+  // route middleware to make sure a user is logged in
+  this.isLoggedIn = function(req, res, next) {
+    if (req.isAuthenticated()) {
+      // if user is authenticated in the session, carry on
+      next();
+    } else {
+      // if they aren't redirect them to the home page
+      res.send(401);
+    }
+  };
 
-	// Route for confirming if user is logged in
-	app.get('/api/user', function(req, res) {
-		res.send(req.isAuthenticated() ? { user: req.user } : { message: 'No user signed in' });
-	});
+  // Route for confirming if user is logged in
+  app.get('/api/user', function(req, res) {
+    res.send(req.isAuthenticated() ? { user: req.user } : { message: 'No user signed in' });
+  });
 
-	// Route to handle local authentication
-	app.post('/api/user', passport.authenticate('local'));
+  // Route to handle local authentication
+  app.post('/api/user', passport.authenticate('local'));
 
-	// Route for Facebook authentication and login
-	app.get('/auth/facebook', passport.authenticate('facebook'));
+  // Route for logout
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
-	// Route to handle the Facebook authentication callback
-	app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-																	successRedirect: '/profile',
-																	failureRedirect: '/'
-																})
-															);
-
-	// Route for Twitter authentication and login
-	app.get('/auth/twitter', passport.authenticate('twitter'));
-
-	// Route to handle the Twitter authentication callback
-	app.get('/auth/twitter/callback', passport.authenticate('twitter', {
-																	successRedirect: '/profile',
-																	failureRedirect: '/'
-																})
-															);
-	
-	// Route for logout
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
-
-	// Catch-all Route
-	app.get('*', function(req, res){
-		res.render('index', {
-			title: 'Boiler' ,
-			user: req.isAuthenticated() ? req.user : null
-		});
-	});
+  // Catch-all Route
+  app.get('*', function(req, res){
+    res.render('index', {
+      title: 'Boiler' ,
+      user: req.isAuthenticated() ? req.user : null
+    });
+  });
 
 };
