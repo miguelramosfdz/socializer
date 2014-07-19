343e01e65dcdeb67013795e7d8cdda9ab7d6ff5b
CJ Ponti <cjponti@gmail.com>
2014-07-19 13:57:33 +0100

    Refactore server.js

--- config/server.js
+++ config/server.js
@@ -1,68 +1,70 @@
-"use strict";
+(function() {
+  "use strict";
 
-// Module dependencies
-var express = require("express"),
-		routes = require("./routes"),
-		http = require("http"),
-		redisStore = require("connect-redis")(express),
-		db = require("./db"),
-		mongoose = require("mongoose"),
-		development = require("./envs/dev");
+  // Module dependencies
+  var http = require("http");
+  var express = require("express");
+  var mongoose = require("mongoose");
+  var socketIO = require("socket.io");
+  
+  // Application dependencies
+  var db = require("./db");
+  var routes = require("./routes");
+  
+  /** Declare app */
+  var app = express();
 
-/** Declare app */
-var app = express();
+  /** Declare port for app */
+  app.set("port", 3000);
 
-/** Declare port for app */
-app.set("port", 3000);
+  /** Declare views engine & folder */
+  app.set("view engine", "jade");
+  app.set("views", __dirname + "/../app/views");
 
-/** Declare views engine & folder */
-app.set("view engine", "jade");
-app.set("views", __dirname + "/../app/views");
+  app.use(express.favicon());
+  app.use(express.logger("dev"));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.set("showStackError", true);
 
-app.use(express.favicon());
-app.use(express.logger("dev"));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.set("showStackError", true);
+  /** Declare public folder */
+  app.use(express.static(__dirname + "/../public"));
 
-/** Declare public folder */
-app.use(express.static(__dirname + "/../public"));
+  // Show error stack
+  app.use(express.errorHandler({showStack: true}));
 
-// Show error stack
-app.use(express.errorHandler({showStack: true}));
+  // Set responce for OPTIONS call
+  app.options("*", function (req, res) {
+    res.send("");
+  });
 
-app.options("*", function (req, res) {
-	res.send("");
-});
+  /** Enable JSONP */
+  app.set("jsonp callback", true);
 
-/** Enable JSONP */
-app.set("jsonp callback", true);
+  app.use(app.router);
 
-app.use(app.router);
+  /** Setup database */
+  db.setup(mongoose);
 
-/** Setup development environment */
-development.setup(app, express);
+  /** Setup routes */
+  routes.setup(app);
 
-/** Setup database */
-db.setup(mongoose);
+  /** Declare server */
+  var server = http.createServer(app);
 
-/** Setup routes */
-routes.setup(app);
+  /** Start app */
+  server.listen(app.get("port"), function() {
+    console.log("Express app listening on port " + app.get("port"));
+  });
 
-/** Declare server */
-var server = http.createServer(app);
+  /** Declare socket */
+  var io = socketIO.listen(server);
 
-/** Start app */
-server.listen(app.get("port"), function() {
-	console.log("Express app listening on port " + app.get("port"));
-});
+  /** Emit message to ensure connection */
+  io.on("connection", function (socket) {
+    socket.emit("connected", { message: "Communicating live from the boiler..."});
+  });
 
-/** Declare socket */
-var io = require("socket.io").listen(server);
+  module.exports = app;
 
-/** Emit message to ensure connection */
-io.on("connection", function (socket) {
-	socket.emit("connected", { message: "Communicating live from the boiler..."});
-})
-
-module.exports = app;
+})();
