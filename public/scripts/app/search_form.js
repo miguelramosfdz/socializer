2e64ab307383c000049efdcaa6e3a8e8e50d0c91
CJ Ponti <cjponti@gmail.com>
2014-08-31 20:08:41 +0100

    Add tweet template to search_form.js

--- public/scripts/app/search_form.js
+++ public/scripts/app/search_form.js
@@ -1,5 +1,17 @@
 define(['jquery', 'backbone'], function($, Backbone) {
 
+  $scope.template = [
+    "<div class='tweet'>",
+       "<div class='tweet-user-image' >",
+         "<img src='{{tweet.user.profile_image_url}}' />",
+        "<div class='tweet-data'>",
+          "<div class='tweet-user'>{{ tweet.user.name }}</div>",
+          "<div class='tweet-text'> {{ tweet.text }}</div>",
+        "</div>",
+      "</div>",
+    "</div>"
+  ].join('');
+  
   $( "#radius" ).keyup(function() {
     $(this).val($scope.radius + ' miles');
   });
