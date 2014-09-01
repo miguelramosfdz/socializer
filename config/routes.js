5e813a1f11634cac37dee4e2a9f3351f077ec291
CJ Ponti <cjponti@gmail.com>
2014-09-01 16:22:21 +0100

    Add Foursquare routes

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,28 @@
 exports.setup = function (server) {
+  "use strict";
 
   var TwitterController = require('../app/controllers/twitter');
   var MainController = require('../app/controllers/main_controller');
-
+  var FoursquareController = require('../app/controllers/foursquare_controller');
+  
   // Serve templates
   server.get('/templates/:type/:name', MainController.serveTemplates);
 
-  
+  /**
+   * Foursquare Authorize routes
+   */
+  server.get('/auth/foursquare', FoursquareController.authorize);
+  server.get('/auth/foursquare/callback', FoursquareController.getCallback);
+
+  /**
+   * Foursquare search routes
+   */
+  server.get('/foursquare/search', FoursquareController.getSearch);
+  server.post('/foursquare/search/user', FoursquareController.postSearch);
+
+  /**
+   * Twitter routes
+   */
   server.get('/twitter/search', TwitterController.getSearch);
   server.post('/twitter/search', TwitterController.postSearch);
 
