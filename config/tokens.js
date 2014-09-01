8ed9a62401299634abd98f8ee1e1c68c4bf6a052
CJ Ponti <cjponti@gmail.com>
2014-09-01 12:15:56 +0100

    Use API_KEY and API_SECRET instead of CONSUMER_KEY and CONSUMER_SECRET; Use apiKey and apiSecret instead of consumerKey and consumerSecret

--- config/tokens.js
+++ config/tokens.js
@@ -3,8 +3,8 @@ module.exports = (function() {
 
   return {
     Twitter: {
-      consumerKey: process.env.TWITTER_CONSUMER_KEY,
-      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      apiKey: process.env.TWITTER_API_KEY,
+      apiSecret: process.env.TWITTER_API_SECRET,
       accessToken: process.env.TWITTER_ACCESS_TOKEN,
       accessSecret: process.env.TWITTER_ACCESS_SECRET 
     }
