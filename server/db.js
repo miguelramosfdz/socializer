72a7da4e60b2e40e88b051786604dd84a62da8ab
CJ Ponti <cjponti@gmail.com>
2014-09-02 18:04:03 +0100

    Refactor db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
 exports.setup = function(mongoose) {
 	'use strict';
 
+  var mongoose = require('mongoose');
   var Hedgehog = require('../.hedgehog.js');
+  var db = Hedgehog.db[process.env.NODE_ENV];
 
-	mongoose.connect(Hedgehog.db[process.env.NODE_ENV], function(err) {
+	mongoose.connect(db, function(err) {
 		if (err) {
       console.log(err);
     } else {
