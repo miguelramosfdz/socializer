934497fa56044cffab00e600cd4c18bbbd6f96e7
CJ Ponti <cjponti@gmail.com>
2014-09-02 18:03:04 +0100

    Wrap user model logic in anonymous function and remove username

--- app/models/User.js
+++ app/models/User.js
@@ -1,68 +1,69 @@
-'use strict';
-
-/**
- * Module dependencies.
- */
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var crypto = require('crypto');
-
-/**
- * @desc User Schema
- * @type {mongoose.Schema}
- */
-var UserSchema = new Schema({
-  email: { type: String, unique: true, required: true },
-  username: { type: String, unique: true, required: true },
-  hashed_password: { type: String, required: true },
-  salt: { type: String, required: true }
-});
-
-/**
- * @desc Set user's salt and hashed_password
- */
-UserSchema.virtual('password').set(function(password) {
-  this.salt = this.makeSalt();
-  this.hashed_password = this.encryptPassword(password);
-});
-
-/**
- * @desc User methods
- * @type {mongoose.Schema.methods}
- */
-UserSchema.methods = {
+module.exports = (function() {
+  'use strict';
 
   /**
-   * @desc Check password
-   * @param {String} password
-   * @return {Boolean}
-   * @api public
+   * Module dependencies.
    */
-  validPassword: function(password) {
-    return this.encryptPassword(password) === this.hashed_password;
-  },
+  var mongoose = require('mongoose');
+  var crypto = require('crypto');
+  
+  /**
+   * @desc User Schema
+   * @type {mongoose.Schema}
+   */
+  var UserSchema = new mongoose.Schema({
+    email: { type: String, unique: true, required: true },
+    hashed_password: { type: String, required: true },
+    salt: { type: String, required: true }
+  });
 
   /**
-   * @desc Make salt
-   * @return {String}
-   * @api public
+   * @desc Set user's salt and hashed_password
    */
-  makeSalt: function() {
-    return crypto.randomBytes(16).toString('base64');
-  },
+  UserSchema.virtual('password').set(function(password) {
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  });
 
   /**
-   * @desc Encrypt password
-   * @param {String} password
-   * @return {String}
-   * @api public
+   * @desc User methods
+   * @type {mongoose.Schema.methods}
    */
-  encryptPassword: function(password) {
-    if (!password || !this.salt) return '';
-    var salt = new Buffer(this.salt, 'base64');
-    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
-  }
+  UserSchema.methods = {
 
-};
+    /**
+     * @desc Check password
+     * @param {String} password
+     * @return {Boolean}
+     * @api public
+     */
+    validPassword: function(password) {
+      return this.encryptPassword(password) === this.hashed_password;
+    },
 
-module.exports = mongoose.model('User', UserSchema);
+    /**
+     * @desc Make salt
+     * @return {String}
+     * @api public
+     */
+    makeSalt: function() {
+      return crypto.randomBytes(16).toString('base64');
+    },
+
+    /**
+     * @desc Encrypt password
+     * @param {String} password
+     * @return {String}
+     * @api public
+     */
+    encryptPassword: function(password) {
+      if (!password || !this.salt) return '';
+      var salt = new Buffer(this.salt, 'base64');
+      return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+    }
+
+  };
+
+  return mongoose.model('User', UserSchema);
+
+})();
