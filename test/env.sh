8939b83d8290e4d20858504377ae5bd1e9aefd47
CJ Ponti <cjponti@gmail.com>
2014-09-03 17:35:58 +0100

    Try env without strings

--- test/env.sh
+++ test/env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ev
 
-export SESSION_SECRET='foobar'
-export FACEBOOK_APP_ID='fooApp'
-export FACEBOOK_APP_SECRET='fooAppSecret'
-export FACEBOOK_CALLBACK_URL='fooCallbackUrl'
-export TWITTER_CONSUMER_KEY='fooConsumserKey'
-export TWITTER_CONSUMER_SECRET='fooConsumserSecret'
-export TWITTER_CALLBACK_URL='fooCallbackUrl'
-export MAILER_SERVICE='fooMailService'
-export MAILER_USERNAME='fooMailUsername'
-export MAILER_PASSWORD='fooMailPassword'
+export SESSION_SECRET=foobar
+export FACEBOOK_APP_ID=fooApp
+export FACEBOOK_APP_SECRET=fooAppSecret
+export FACEBOOK_CALLBACK_URL=fooCallbackUrl
+export TWITTER_CONSUMER_KEY=fooConsumserKey
+export TWITTER_CONSUMER_SECRET=fooConsumserSecret
+export TWITTER_CALLBACK_URL=fooCallbackUrl
+export MAILER_SERVICE=fooMailService
+export MAILER_USERNAME=fooMailUsername
+export MAILER_PASSWORD=fooMailPassword
