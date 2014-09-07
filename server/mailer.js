6b76ccf9f22372de197decb8abfba545b318eb27
CJ Ponti <cjponti@gmail.com>
2014-09-07 17:29:50 +0100

    Remove Hedgehog variable as it is now in GLOBAL & refactor

--- server/mailer.js
+++ server/mailer.js
@@ -1,46 +1,33 @@
-module.exports= (function() {
+var nodemailer = require('nodemailer');
 
-  var Hedgehog = require('../.hedgehog');
-  var nodemailer = require('nodemailer');
-  var transport = {
-      service: Hedgehog.mailer.service,
-      auth: {
-          user: Hedgehog.mailer.username,
-          pass: Hedgehog.mailer.password
-      }
-  };
-
-  var mailer =  {
-
-    /**
-     * Create reusable transporter object using SMTP transport
-     */
-    transporter: nodemailer.createTransport(transport),
-
-    /**
-     * Send mail with defined transport object
-     * @param {Array} email List of receivers
-     * @param {String} email Subject line
-     * @param {String} email Plain text body
-     * @param {String} email HTML body
-     */
-    sendMail: function(email, subject, text, html) {
-      mailer.transporter.sendMail({
-          from: Hedgehog.mailer.email,
-          to: email,
-          subject: subject,
-          text: text,
-          html: html
-      }, mailer.sendMailCallback);
-    },
-
-    sendMailCallback: function(error, info){
-      if (error) return console.log(error);
-      console.log('Message sent: ' + info.response);
+var transport = {
+    service: Hedgehog.mailer.service,
+    auth: {
+        user: Hedgehog.mailer.username,
+        pass: Hedgehog.mailer.password
     }
+};
 
-  };
+exports.transporter = nodemailer.createTransport(transport);
 
-  return mailer;
-  
-})();
+/**
+ * Send mail with defined transport object
+ * @param {Array} email List of receivers
+ * @param {String} email Subject line
+ * @param {String} email Plain text body
+ * @param {String} email HTML body
+ */
+exports.sendMail = function(email, subject, text, html) {
+  mailer.transporter.sendMail({
+      from: Hedgehog.mailer.email,
+      to: email,
+      subject: subject,
+      text: text,
+      html: html
+  }, mailer.sendMailCallback);
+};
+
+exports.sendMailCallback = function(error, info) {
+  if (error) return console.log(error);
+  console.log('Message sent: ' + info.response);
+};
