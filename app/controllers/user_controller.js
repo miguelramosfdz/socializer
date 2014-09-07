05e621f782f08166770d792598aeb1b6fac27a97
CJ Ponti <cjponti@gmail.com>
2014-09-07 17:28:10 +0100

    Add social unlink functions and unlinkCallback function

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -1,16 +1,28 @@
 module.exports = (function() {
 
-  var async = require('async');
+  var async = require("async");
+
+  var unlinkCallback = function(social_network, req, res) {
+    return function(err) {
+      if (err) {
+        req.flash("error", social_network+" account could not be unlinked.");
+        res.redirect("/account");
+      } else{
+        req.flash("success", social_network+" account unlinked!");
+        res.redirect("/account");
+      }
+    };
+  };
 
   return {
-    getSignUp: function(req, res, next) {
-      res.render('users/sign_up');
+    getSignUp: function(req, res) {
+      res.render("users/sign_up");
     },
-    getLogIn: function(req, res, next) {
-      res.render('users/sign_in');
+    getLogIn: function(req, res) {
+      res.render("users/sign_in");
     },
-    getAccount: function(req, res, next) {
-      res.render('users/account');
+    getAccount: function(req, res) {
+      res.render("users/account");
     },
     resetPassword: function(req, res, next) {
       var validPassword = req.user.validPassword(req.body.current_password);
@@ -19,18 +31,18 @@ module.exports = (function() {
       async.waterfall([
         function(done) {
            if (!validPassword) {
-            req.flash('error', 'You did not enter a valid password');
+            req.flash("error", "You did not enter a valid password");
             done();
           } else if (!match) {
-            req.flash('error', 'The passwords you entered did not match');
+            req.flash("error", "The passwords you entered did not match");
             done();
           } else {
-            req.user.set('password', req.body.password);
+            req.user.set("password", req.body.password);
             req.user.save(function(err) {
               if (err) {
-                req.flash('error', err);
+                req.flash("error", err);
               } else {
-                req.flash('success', 'Your password has been changed.');  
+                req.flash("success", "Your password has been changed.");  
               }
               done();
             });
@@ -38,10 +50,37 @@ module.exports = (function() {
         }
       ], 
       function(err) {
-        if (err) return next(err);
-        res.redirect('/account');
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/account");
       });
+    },
+
+    unlinkTwitter: function(req, res) {
+      var user = req.user;
+      user.twitter.token = undefined;
+      user.save(unlinkCallback("Twitter", req, res));
+    },
+
+    unlinkFacebook: function(req, res) {
+      var user = req.user;
+      user.facebook.token = undefined;
+      user.save(unlinkCallback("Facebook", req, res));
+    },
+
+    unlinkGoogle: function(req, res) {
+      var user = req.user;
+      user.google.token = undefined;
+      user.save(unlinkCallback("Google", req, res));
+    },
+
+    unlinkFoursquare: function(req, res) {
+      var user = req.user;
+      user.foursquare.token = undefined;
+      user.save(unlinkCallback("Foursquare", req, res));
     }
+    
   };
 
 })();
