9d3a51bb34b2b9442da798e608e269d61a8cb550
CJ Ponti <cjponti@gmail.com>
2014-09-07 17:30:22 +0100

    Fix foursquare authentication

--- server/oauth/foursquare.js
+++ server/oauth/foursquare.js
@@ -1,28 +1,87 @@
 var REST = require('restler');
-var Keys = require('../../.hedgehog').oauth.Foursquare;
 
-var oauth_url = "https://foursquare.com/oauth2/";
+// exports.Foursquare = function Foursquare(keys) {
+//   if (!keys.client_id) throw new Error("Foursquare requires client_id");
+//   if (!keys.client_secret) throw new Error("Foursquare requires client_secret");
+//   if (!keys.redirect_uri) throw new Error("Foursquare requires redirect_uri");
+//   this.base_url = 
+//   this.client_id = keys.client_id;
+//   this.client_secret = keys.client_secret;
+//   this.redirect_uri = keys.redirect_uri;
+// };
+
+// exports.Foursquare.prototype.authenticate = function() {
+
+// };
 
 var Foursquare = {
 
-  authenticate: function() {
-    REST.get(oauth_url+"authenticate", {
-      response_type: "code",
-      client_id: Keys.client_id,
-      redirect_uri: Keys.callback_url
-    }).on('complete', function(data) {
-      console.log(data);
+  keys: require('../../.hedgehog').oauth.Foursquare,
+
+  base_url: "https://foursquare.com/oauth2/",
+
+  profileUrl: "https://api.foursquare.com/v2/users/self",
+
+  version: "&v=20140806",
+
+  authenticate: function(req, res, next) {
+    res.redirect([
+      Foursquare.base_url,"authenticate?",
+      "response_type=code",
+      "&client_id=", Foursquare.keys.client_id,
+      "&redirect_uri=", Foursquare.keys.callback_url
+    ].join(''));
+  },
+
+  getRequestToken: function(req, res, next) {
+    REST.get([
+      Foursquare.base_url,"access_token?",
+      "code=", req.query.code,
+      "&client_id=", Foursquare.keys.client_id,
+      "&client_secret=", Foursquare.keys.client_secret,
+      "&redirect_uri=", Foursquare.keys.callback_url,
+      "&grant_type=authorization_code"
+    ].join('')).on('complete', function(data) {
+      if (data.access_token) {
+        var user = req.user;
+        user.foursquare.token = data.access_token;
+        user.save(function(err) {
+          if (err) {
+            return err;
+          }
+          req.flash("success", "Foursquare account authenticated!");
+          res.redirect("/account");
+        });
+      } else {
+        req.flash("error", "Could not authenticate Foursquare account.");
+        res.redirect("/account");
+      }
     });
   },
 
-  getRequestToken: function(code, cb) {
-    REST.get(oauth_url+"access_token", {
-      code: code,
-      client_id: Keys.client_id,
-      client_secret: Keys.client_secret,
-      redirect_uri: redirect_uri,
-      grant_type: "authorization_code"
-    }).on('complete', cb);
+  getProfile: function(req, res, next) {
+    REST.get([
+      Foursquare.profileUrl,
+      "?oauth_token=",req.user.foursquare.token,
+      Foursquare.version
+    ].join("")).on("complete", function(data) {
+      if (data.response.user) {
+        var user = req.user;
+        user.foursquare.profile = data.response.user;
+        user.save(function(err) {
+          if (err) {
+            req.flash("error", "Foursquare profile could not be saved.");
+            res.redirect("/account");
+          } else {
+            req.flash("success", "Foursquare profile saved!");
+            res.redirect("/account");
+          }
+        });
+      } else {
+        req.flash("error", "Foursquare profile could not be found.");
+        res.redirect("/account");
+      }
+    });
   }
 
 };
