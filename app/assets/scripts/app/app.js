e7eddad0fcd54c59b402c56a158ff6a8ce38ae85
CJ Ponti <cjponti@gmail.com>
2014-09-18 00:03:51 +0100

    Add API, resetAppView, and setCurrentView to App model

--- app/assets/scripts/app/app.js
+++ app/assets/scripts/app/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var App = Backbone.Model.extend({
 
   collections: {},
@@ -8,7 +10,25 @@ var App = Backbone.Model.extend({
 
   currentView: null,
   
-  Router: require("./router")
+  Router: require("./router"),
+
+  API: require("./service/api"),
+
+  resetAppView: function(html) {
+    $("#content").html(html);
+  },
+
+  setCurrentView: function(view) {
+    if (this.currentView) {
+      console.log('Destroy view:'+view.viewId);
+      var currentView = this.currentView;
+      currentView.collection.unbind();
+      currentView.remove();
+      currentView.unbind();
+    }
+    console.log('Setting currentView to: '+view.viewId);
+    this.currentView = view;
+  }
 
 });
 
