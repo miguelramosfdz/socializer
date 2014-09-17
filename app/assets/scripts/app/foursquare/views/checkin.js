d9faa2a86776499bebe34b0f76096a46bf8c7877
CJ Ponti <cjponti@gmail.com>
2014-09-17 18:07:59 +0100

    Refactor CheckinView to add tagName, className, and edit template

--- app/assets/scripts/app/foursquare/views/checkin.js
+++ app/assets/scripts/app/foursquare/views/checkin.js
@@ -2,18 +2,27 @@ module.exports = (function() {
 
   var CheckinView = Backbone.View.extend({
 
+    tagName: 'li',
+
+    className: 'list-group-item',
+
     template: _.template([
-      "<li class='list-group-item'>",
-        "<div class='row'>",
-          "<div class='col-md-offset-3 col-md-6'>",
-            "<%= venue.name %> in <%= venue.location.city %>, <%= venue.location.country %>",
-          "</div>",
+      "<div class='row'>",
+        "<div class='col-md-offset-3 col-md-6'>",
+          "<%= venue.name %> in <%= venue.location.city %>, <%= venue.location.country %>",
         "</div>",
-      "</li>"
+      "</div>"
     ].join('')),
 
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.bind('change', this.render);
+    },
+
     render: function() {
-      return this.template(this.model.toJSON());
+      this.el.dataset.id = this.model.get("id");
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this.el;
     }
 
   });
