7a0fda439dfdd8e67ce898557e863f2546943047
CJ Ponti <cjponti@gmail.com>
2014-09-20 21:08:48 +0100

    Refactor repos template and add logic to render

--- app/assets/scripts/app/github/views/repos.js
+++ app/assets/scripts/app/github/views/repos.js
@@ -7,19 +7,22 @@ var ReposView = Backbone.View.extend({
   className: "list-group",
 
   template: _.template(
-    '<li class="list-group-item">'+
-      '<%= name %>'+
-    '</li>'
+    '<% _.each(repos, function(repo) { %>'+
+      '<li class="list-group-item">'+
+        '<h4><%= repo.get("name") %></h4>'+
+      '</li>'+
+    '<% }) %>'
   ),
 
   collection: App.Github.collection.Repos,
 
-  initialize: function() {
-
-  },
-
   render: function() {
-
+    var self = this;
+    this.listenTo(this.collection, "change reset add remove", function() {
+      $("#repos").html(this.$el.html(self.template({
+        repos: App.Github.collection.Repos.models 
+      })));
+    });
   }
 
 });
