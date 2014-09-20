4bc4a98a820e3882de211a388c5fb6783d77df23
CJ Ponti <cjponti@gmail.com>
2014-09-20 18:59:56 +0100

    Use *.collection instead of *.store, set Repos and Issues collections to App

--- app/assets/scripts/app/github/app.js
+++ app/assets/scripts/app/github/app.js
@@ -1,22 +1,37 @@
 "use strict";
 
+var MainView = require("./views/main");
+
 module.exports = {
 
   initialize: function() {
     App.Github = {
-      store: {},
+      collection: {},
       model: {},
       view: {}
     };
     
-    App.Github.store.Repos = require('./collection/repos');
-    App.Github.store.Repos.load();
+    App.Github.collection.Repos = require('./collection/repos');
+    App.Github.collection = require("../collection/issues");
+    
+    /**
+     * Fetch current user's repos
+     */
+    App.Github.collection.Repos.fetch();
+
+    /**
+     * Render main view of Github application
+     */
+    MainView.render();
+
+    /**
+     * Set currentView of App to github
+     */
+    App.setCurrentView(MainView);
   },
 
   issues: function() {
-    var MainView = require("./views/main");
-    MainView.render();
-    App.setCurrentView(MainView);
+
   }
 
 };
