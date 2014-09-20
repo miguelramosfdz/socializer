f87bc88da27c06b1a171bf90d02858402f7ac2ea
CJ Ponti <cjponti@gmail.com>
2014-09-20 17:53:12 +0100

    Create RepoIssuesView

---
+++ app/assets/scripts/app/github/views/repo_issues.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var Repos = require("../collection/repos");
+var IssueView = require("./issue");
+
+var RepoIssuesView = Backbone.View.extend({
+  
+  collection: Repos,
+
+  template: _.template(
+    "<div class='panel panel-default>"+
+      "<div class='panel-heading'>"+
+        "<h3><%= name %></h3>"+
+      "</div>"+
+      "<ul class='list-group'>"+
+        "<% _.each(issues, function(issue) { %>"+
+          "<%= issue.render() %>"+
+        "<% }) %>"+
+      "</ul>"+
+    "</div>"
+  ),
+
+  initialize: function() {
+    this.collection.on('add', this.render);
+  },
+
+  render: function() {
+    this.template({ 
+      repos: this.collection.models
+    });
+  }
+
+});
+
+module.exports = new RepoIssuesView();
